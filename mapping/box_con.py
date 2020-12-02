eaa17f5c037b4da8bca4c379365f5ecde634f09c
nilaarta <37109093+nilaarta@users.noreply.github.com>
2020-12-02 11:22:48 +0800

    update token

--- mapping/box_con.py
+++ mapping/box_con.py
@@ -3,6 +3,6 @@ from boxsdk import OAuth2, Client
 auth = OAuth2(
     client_id='c0hjuh3tjr90sloycc4g0jcdvid1yjs2',
     client_secret='w5oJLroCuN4TQ8bEjmJycwFmOexZXB2g',
-    access_token='WLv7WCIvA296Cw9PUm7XxfvfXg1PNtV5',
+    access_token='cdz17PensWyoG97yZ7jgRNhUMkGeOzkX',
 )
 client = Client(auth)
--- penerima_mapping/box_con.py
+++ penerima_mapping/box_con.py
@@ -3,6 +3,6 @@ from boxsdk import OAuth2, Client
 auth = OAuth2(
     client_id='c0hjuh3tjr90sloycc4g0jcdvid1yjs2',
     client_secret='w5oJLroCuN4TQ8bEjmJycwFmOexZXB2g',
-    access_token='x0FnsMKioj4UMGlvucgkdliqUXIVb5Uo',
+    access_token='cdz17PensWyoG97yZ7jgRNhUMkGeOzkX',
 )
 client = Client(auth)
